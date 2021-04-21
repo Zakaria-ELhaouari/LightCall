edea761a8a45f4e2cd3c34775ee603790e8f5422
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-04-21 10:03:58 +0000

    added template assets and made a valid layout

---
+++ client-app/src/app/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Layout = () => {
+    return (
+        
+            <div className="main-wrapper">
+                <div className="navbar-bg"></div>
+                <Header/>
+            </div>
+        
+    )
+}
+
+export default Layout
